7a7ba479bc57292399d233005c2ebfdb7ca3ffd3
leesj0531 <77476111+leesj0531@users.noreply.github.com>
2022-06-08 18:07:40 +0900

    Update clip.py

--- clip/clip.py
+++ clip/clip.py
@@ -76,15 +76,24 @@ def _convert_image_to_rgb(image):
     return image.convert("RGB")
 
 
-def _transform(n_px):
-    return Compose([
-        ToPILImage(),
-        Resize(n_px, interpolation=BICUBIC),
-        CenterCrop(n_px),
-        _convert_image_to_rgb,
-        ToTensor(),
-        Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
-    ])
+def _transform(n_px, to_pil = False):
+    if to_pil:
+        return Compose([
+            ToPILImage(),
+            Resize(n_px, interpolation=BICUBIC),
+            CenterCrop(n_px),
+            _convert_image_to_rgb,
+            ToTensor(),
+            Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+        ])
+    else:
+        return Compose([
+            Resize(n_px, interpolation=BICUBIC),
+            CenterCrop(n_px),
+            _convert_image_to_rgb,
+            ToTensor(),
+            Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+        ])
 
 
 def available_models() -> List[str]:
